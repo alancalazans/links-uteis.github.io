87e69fdb7ecac90757e22f014656b8efdcf3397b
alancalazans <alan2calazans@gmail.com>
2023-07-29 09:26:00 -0300

    2023.07.29 09:25

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,16 @@
-var categoria="Pesquisa",
+var categoria = "Pesquisa",
+		dados,
 		menu,
 		sites,
-		dados;
-
-getJson = (file) => {
-	return fetch(file)
-	.then(res => res.json())
-	.then(json => {
-		return json;
-	})
-	.catch(error => {
-		console.log(error.message);
-	});
-}
-
-goSites = () => {
-	fetch('assets/templates/sites.jade')
-	.then(response => response.text())
-	.then(html => {
-		dados = getJson('assets/json/sites.json');
-		dados.then(json => {
-			sites = json;
-			document.getElementById('sites').innerHTML = jade.compile(html)();
-		});
-	})
-	.catch(error => {
-		console.log(error.message);
-	});
-}
-
-setAtributo = () => {
-	document.getElementsByClassName(categoria)[0].setAttribute('class',categoria+' active');
-}
-
-removeAtributo = () => {
-	document.getElementsByClassName(categoria)[0].setAttribute('class',categoria);
-}
-
-clickCategoria = (el) => {
-	removeAtributo();
-	categoria = el.getAttribute('class');
-	setAtributo();
-	goSites();
-}
+		/* module */
+		clickCategory  = aux.clickCategory,
+		getJson        = aux.getJson,
+		goSites        = aux.goSites,
+		removeAtribute = aux.removeAtribute,
+		screenAdjust   = aux.screenAdjust,		
+		setAtribute    = aux.setAtribute;
+		
+screenAdjust();
 
 fetch('assets/templates/menu.jade')
 .then(response => response.text())
@@ -51,7 +19,7 @@ fetch('assets/templates/menu.jade')
 	dados.then(json => {
 		menu = json;
 		document.getElementById('menu').innerHTML = jade.compile(html)();
-		setAtributo();
+		setAtribute();
 	});
 })
 .catch(error => {
---
+++ assets/js/module/aux.js
@@ -0,0 +1,76 @@
+const aux = {
+  /*----------------------------------------
+   * screenAjust()
+   *----------------------------------------*/
+  screenAdjust: () => {
+    var windowWidth, windowHeight;
+    //var screenWidth, screenHeight;
+    
+    const sizeOfThings = () => {
+    	windowWidth = window.innerWidth;
+    	windowHeight = window.innerHeight;
+    	//screenWidth = screen.width;
+    	//screenHeight = screen.height;
+    
+    	document.getElementById('container').style.height = (windowHeight-10).toString() + 'px';
+    	document.getElementById('container').style.width = (windowWidth-100).toString() + 'px';
+    	document.getElementById('menu').style.height = (windowHeight-10).toString() + 'px';
+    	document.getElementById('sites').style.height = (windowHeight-10).toString() + 'px';
+    };
+    
+    sizeOfThings();
+    
+    window.addEventListener('resize', () => { sizeOfThings() });    
+  },
+  /*----------------------------------------
+   * getJson(file)
+   *----------------------------------------*/
+  getJson: (file) => {
+  	return fetch(file)
+  	.then(res => res.json())
+  	.then(json => {
+  		return json;
+  	})
+  	.catch(error => {
+  		console.log(error.message);
+  	});
+  },
+  /*----------------------------------------
+   * setAtribute()
+   *----------------------------------------*/
+  setAtribute: () => {
+  	document.getElementsByClassName(categoria)[0].setAttribute('class',categoria+' active');
+  },
+  /*----------------------------------------
+   * removeAtribute()
+   *----------------------------------------*/
+  removeAtribute: () => {
+  	document.getElementsByClassName(categoria)[0].setAttribute('class',categoria);
+  },
+  /*----------------------------------------
+   * clickCategory(el)
+   *----------------------------------------*/
+  clickCategory: (el) => {
+  	removeAtribute();
+  	categoria = el.getAttribute('class');
+  	setAtribute();
+  	goSites();
+  },
+  /*----------------------------------------
+   * goSites()
+   *----------------------------------------*/
+  goSites: () => {
+  	fetch('assets/templates/sites.jade')
+  	.then(response => response.text())
+  	.then(html => {
+  		dados = getJson('assets/json/sites.json');
+  		dados.then(json => {
+  			sites = json;
+  			document.getElementById('sites').innerHTML = jade.compile(html)();
+  		});
+  	})
+  	.catch(error => {
+  		console.log(error.message);
+  	});
+  }
+}
