77c35bca6ec87320e81730b5ab407041cf1abe3b
alancalazans <alan2calazans@gmail.com>
2024-11-21 12:45:17 -0300

    2024.11.21 12:45

---
+++ assets/html/radios.js
@@ -0,0 +1,9 @@
+const radiosJson = [
+	{ name: "Alvorada FM 94.9", url: "http://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_ALVORADAAAC.aac" },
+	{ name: "Antena 1", url: "https://antenaone.crossradio.com.br/stream/1;" },
+	{ name: "Brazilian FM", url: "http://s17.maxcast.com.br:8228/live" },
+	{ name: "CDL FM", url: "https://9595.brasilstream.com.br/stream" },
+	{ name: "Chérie FM", url: "https://scdn.nrjaudio.fm/fr/30201/mp3_128.mp3?cdn_path=audio_lbs10&access_token=370f0224453d44c0ab712305062257f4" },
+	{ name: "Nostalgie", url: "https://edge08.streamonkey.net/nostalgie-live/stream/mp3" }
+];
+
