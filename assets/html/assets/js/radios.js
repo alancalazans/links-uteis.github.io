b950b1e2f477c4c097b47389bd1c1c34cdb098de
alancalazans <alan2calazans@gmail.com>
2024-11-22 15:11:38 -0300

    2024.11.22 15:11

--- assets/html/assets/js/radios.js
+++ assets/html/assets/js/radios.js
@@ -4,6 +4,10 @@ const radiosJson = [
 	{ name: "Brazilian FM", url: "http://s17.maxcast.com.br:8228/live" },
 	{ name: "CDL FM", url: "https://9595.brasilstream.com.br/stream" },
 	{ name: "Chérie FM", url: "https://scdn.nrjaudio.fm/fr/30201/mp3_128.mp3?cdn_path=audio_lbs10&access_token=370f0224453d44c0ab712305062257f4" },
-	{ name: "Nostalgie", url: "https://edge08.streamonkey.net/nostalgie-live/stream/mp3" }
+	{ name: "Destinos em Músicas 80 e 90", url: "https://sv11.hdradios.net:6868/;" },
+	{ name: "Educasom FM", url: "https://s30.maxcast.com.br:8036/live" },
+	{ name: "Jovem Pan FM", url: "https://r13.ciclano.io:15159/live" },
+	{ name: "Nostalgie", url: "https://edge08.streamonkey.net/nostalgie-live/stream/mp3" },
+	{ name: "Transamérica Pop", url: "https://playerservices.streamtheworld.com/api/livestream-redirect/RT_RJAAC.aac" }
 ];
 
