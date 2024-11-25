749276ea2fb85a7ae0192a4000493bfd9dbaf8c4
alancalazans <alan2calazans@gmail.com>
2024-11-25 12:25:20 -0300

    2024.11.25 12:25

--- assets/html/assets/js/radios.js
+++ assets/html/assets/js/radios.js
@@ -5,9 +5,15 @@ const radiosJson = [
 	{ name: "CDL FM", url: "https://9595.brasilstream.com.br/stream" },
 	{ name: "Chérie FM", url: "https://scdn.nrjaudio.fm/fr/30201/mp3_128.mp3?cdn_path=audio_lbs10&access_token=370f0224453d44c0ab712305062257f4" },
 	{ name: "Destinos em Músicas 80 e 90", url: "https://sv11.hdradios.net:6868/;" },
+	{ name: "Gazeta FM (RS)", url: "https://radio.hostspry.com.br/8002/stream" },
+	{ name: "Interativa FM (SP)", url: "https://r6.ciclano.io:15090/stream" },
 	{ name: "Jovem Pan FM (BH)", url: "https://r13.ciclano.io:15115/stream" },
+	{ name: "Minuano FM (RS)", url: "https://audiostream.i9suaradio.com:1415/stream" },
+	{ name: "Nativa (RS)", url: "https://r16.ciclano.io:15026/stream" },
 	{ name: "Nostalgie", url: "https://edge08.streamonkey.net/nostalgie-live/stream/mp3" },
+	{ name: "Pop Rock FM (RS)", url: "https://cast2.youngtech.radio.br:8160/radio" },
 	{ name: "Transamérica Pop", url: "https://playerservices.streamtheworld.com/api/livestream-redirect/RT_RJAAC.aac" },
-	{ name: "UFMG", url: "http://150.164.63.210:8000/aovivo.mp3" }
+	{ name: "UFMG", url: "http://150.164.63.210:8000/aovivo.mp3" },
+	{ name: "Xangri-lá FM (RS)", url: "http://cast4.audiostream.com.br:8659/mp3" }
 ];
 
