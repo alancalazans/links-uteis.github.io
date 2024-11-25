3936c80cd0b6aaedbf2c1e7e0e8a5da841b0ffcd
alancalazans <alan2calazans@gmail.com>
2024-11-25 12:00:58 -0300

    2024.11.25 12:00

--- assets/html/assets/js/radios.js
+++ assets/html/assets/js/radios.js
@@ -1,13 +1,13 @@
 const radiosJson = [
 	{ name: "Alvorada FM 94.9", url: "http://playerservices.streamtheworld.com/api/livestream-redirect/RADIO_ALVORADAAAC.aac" },
-	{ name: "Antena 1", url: "https://antenaone.crossradio.com.br/stream/1;" },
+	{ name: "Antena 1", url: "https://antenaone.crossradio.com.br/stream/; " },
 	{ name: "Brazilian FM", url: "http://s17.maxcast.com.br:8228/live" },
 	{ name: "CDL FM", url: "https://9595.brasilstream.com.br/stream" },
 	{ name: "Chérie FM", url: "https://scdn.nrjaudio.fm/fr/30201/mp3_128.mp3?cdn_path=audio_lbs10&access_token=370f0224453d44c0ab712305062257f4" },
 	{ name: "Destinos em Músicas 80 e 90", url: "https://sv11.hdradios.net:6868/;" },
-	{ name: "Educasom FM", url: "https://s30.maxcast.com.br:8036/live" },
-	{ name: "Jovem Pan FM", url: "https://r13.ciclano.io:15159/live" },
+	{ name: "Jovem Pan FM (BH)", url: "https://r13.ciclano.io:15115/stream" },
 	{ name: "Nostalgie", url: "https://edge08.streamonkey.net/nostalgie-live/stream/mp3" },
-	{ name: "Transamérica Pop", url: "https://playerservices.streamtheworld.com/api/livestream-redirect/RT_RJAAC.aac" }
+	{ name: "Transamérica Pop", url: "https://playerservices.streamtheworld.com/api/livestream-redirect/RT_RJAAC.aac" },
+	{ name: "UFMG", url: "http://150.164.63.210:8000/aovivo.mp3" }
 ];
 
